c33749b409d56655978dc6f7fc48aa94b9d13113
Anton <antonpaper500@gmail.com>
2025-01-17 12:13:25 +0300

    added auth middleware

--- services/token-service.js
+++ services/token-service.js
@@ -11,6 +11,24 @@ class TokenService {
     }
   }
 
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
   async saveToken(userId, refreshToken) {
     const tokenData = await Token.findOne({ where: { userId: userId } })
     if (tokenData) {
@@ -22,6 +40,16 @@ class TokenService {
     return token
   }
 
+  async deleteToken(refreshToken) {
+    const tokenData = await Token.destroy({ where: { refreshToken } });
+    return tokenData
+  }
+
+
+  async findToken(refreshToken) {
+    const tokenData = await Token.findOne({ where: { refreshToken } });
+    return tokenData
+  }
 }
 
 module.exports = new TokenService()
